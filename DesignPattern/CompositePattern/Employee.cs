bf09058d578ebaf7f51bade4d93b4663417382cd
Dindin Dhino Alamsyah <dhino.alamsyah@geekseat.com.au>
2022-12-21 15:11:30 +0700

    Add composite pattern

---
+++ DesignPattern/CompositePattern/Employee.cs
@@ -0,0 +1,36 @@
+﻿public class Employee
+{
+    private string name;
+    private string dept;
+    private int salary;
+    private List<Employee> subordinates;
+
+    public Employee(string name, string dept, int salary)
+    {
+        this.name = name;
+        this.dept = dept;
+        this.salary = salary;
+        subordinates = new List<Employee>();
+    }
+
+    public void Add(Employee employee)
+    {
+        subordinates.Add(employee);
+    }
+
+    public void Remote(Employee employee)
+    {
+        subordinates.Remove(employee);
+    }
+
+    public List<Employee> GetSubordinates()
+    {
+        return subordinates;
+    }
+
+    public string toString()
+    {
+        return ("Employee :[ Name : " + name 
+				+ ", dept : " + dept + ", salary :" + salary + " ]");
+    }
+}
---
+++ DesignPattern/CompositePattern/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+Employee CEO = new Employee("John", "CEO", 30000);
+
+Employee headSales = new Employee("Robert", "Head Sales", 20000);
+
+Employee headMarketing = new Employee("Michel", "Head Marketing", 20000);
+
+Employee clerk1 = new Employee("Laura", "Marketing", 10000);
+Employee clerk2 = new Employee("Bob", "Marketing", 10000);
+
+Employee salesExecutive1 = new Employee("Richard", "Sales", 10000);
+Employee salesExecutive2 = new Employee("Rob", "Sales", 10000);
+
+CEO.Add(headSales);
+CEO.Add(headMarketing);
+
+headSales.Add(salesExecutive1);
+headSales.Add(salesExecutive2);
+
+headMarketing.Add(clerk1);
+headMarketing.Add(clerk2);
+
+Console.WriteLine(CEO.toString());
+foreach(Employee headEmployee in CEO.GetSubordinates())
+{
+    Console.WriteLine("->" + headEmployee.toString());
+
+    foreach(Employee employee in headEmployee.GetSubordinates())
+    {
+        Console.WriteLine("->->" + employee.toString());
+    }
+}
+
+Console.ReadLine();
