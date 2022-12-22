b5ce9e867ae0ac8a9189ffe74a4120df4312db87
Dindin Dhino Alamsyah <dhino.alamsyah@geekseat.com.au>
2022-12-22 16:34:31 +0700

    Add Proxy Pattern

---
+++ DesignPattern/ProxyPattern/ConcreteClasses.cs
@@ -0,0 +1,40 @@
+﻿public class RealImage : Image
+{
+    private string fileName;
+
+    public RealImage(string fileName)
+    {
+        this.fileName = fileName;
+        loadFromDisk(fileName);
+    }
+
+    private void loadFromDisk(string fileName)
+    {
+        Console.WriteLine("Loading " + fileName);
+    }
+
+    public void Display()
+    {
+        Console.WriteLine("Displaying " + fileName);
+    }
+}
+
+public class ProxyImage : Image
+{
+    private RealImage realImage;
+    private string fileName;
+
+    public ProxyImage(string fileName)
+    {
+        this.fileName = fileName;
+    }
+
+    void Image.Display()
+    {
+        if (realImage == null)
+        {
+            realImage = new RealImage(fileName);
+        }
+        realImage.Display();
+    }
+}
---
+++ DesignPattern/ProxyPattern/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Image image = new ProxyImage("test_10mb.jpg");
+Console.WriteLine("Image will be loaded from disk...");
+image.Display();
+
+Console.WriteLine("\nImage will not be loaded from disk...");
+image.Display();
+
+Console.ReadLine();
