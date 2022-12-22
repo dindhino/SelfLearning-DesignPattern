4c6d30b87ceb756d3968ae692d22e306f845c2e2
Dindin Dhino Alamsyah <dhino.alamsyah@geekseat.com.au>
2022-12-22 15:20:23 +0700

    Add Flyweight Pattern

---
+++ DesignPattern/FlyweightPattern/Factory.cs
@@ -0,0 +1,18 @@
+﻿public class ShapeFactory
+{
+    private static readonly Dictionary<string, Circle> circleMap = new Dictionary<string, Circle>();
+
+    public static Shape GetCircle(string color)
+    {
+        Circle circle = (Circle)circleMap.GetValueOrDefault(color);
+
+        if (circle == null)
+        {
+            circle = new Circle(color);
+            circleMap.Add(color, circle);
+            Console.WriteLine("Creating circle of color : " + color);
+        }
+
+        return circle;
+    }
+}
---
+++ DesignPattern/FlyweightPattern/Program.cs
@@ -0,0 +1,25 @@
+﻿internal class FlyweightPatternDemo
+{
+    private static readonly List<string> colors = new List<string> { "Red", "Green", "Blue", "White", "Black" };
+    private static readonly Random random = new Random();
+    private static string getRandomColor()
+    {
+        return colors[(int)(random.Next(0, colors.Count))];
+    }
+    private static int getRandomXY()
+    {
+        return (int)(random.Next(0, 100));
+    }
+    private static void Main(string[] args)
+    {
+        for (int i = 0; i < 20; ++i)
+        {
+            Circle circle = (Circle)ShapeFactory.GetCircle(getRandomColor());
+            circle.SetX(getRandomXY());
+            circle.SetY(getRandomXY());
+            circle.SetRadius(100);
+            circle.Draw();
+        }
+        Console.ReadLine();
+    }
+}
